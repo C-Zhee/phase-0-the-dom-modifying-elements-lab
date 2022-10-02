b91689f194abecfc7b7242cac27308a0803cbceb
Corey <coreyzhee@gmail.com>
2022-10-02 04:44:03 -0400

    commit tree 125bf73a8f6194df1dae0275a329e0f115fddad2

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// Write your code here!
+document.getElementById('main').remove();
+
+const newHeader = document.createElement('h1')
+newHeader.id = 'victory'
+newHeader.innerHTML = "Corey is the champion"
+
+it("has a 'newHeader' variable that points to node 'h1#victory'", () => {
+  console.log(newHeader)
+  expect(newHeader.nodeName, "Make sure you create an <h1> with id 'victory'").eql('H1')
+});
